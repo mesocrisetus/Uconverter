ecd253bf1eaa20fe3df7613756661dc2d08765d7
Mesocrisetus <alexander.bolivardiaz@gmail.com>
2025-01-15 17:55:38 -0500

    App was migrated to webApp

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask,render_template,request,send_from_directory
+import os
+import functions as fn
+
+app = Flask(__name__)
+app.secret_key =os.urandom(24)
+
+@app.route("/")
+def index():
+    
+    return render_template('index.html')
+
+
+@app.route('/descargarVideo', methods=['POST'])
+def descargarVideo():
+    url = request.form['url']
+    format = request.form['format']
+    path = fn.convertir(url,format)
+    if path:
+        filename = os.path.basename(path)
+        return render_template('index.html',filename=filename,format=format)
+    else:
+        return "Error al convertir el video", 500
+
+@app.route('/download/<filename>')
+def download_file(filename):
+    folder = os.path.join(os.getcwd(), 'vids') 
+    return send_from_directory(folder, filename, as_attachment=True)
+
+if __name__ == '__main__':
+    app.run(debug=True) 
---
+++ functions.py
@@ -0,0 +1,57 @@
+from pytubefix import YouTube
+from pytubefix.cli import on_progress
+import os,shutil
+
+
+
+def convertir(url,format):
+    cPath = os.getcwd()
+    folder = 'vids'
+    path = os.path.join(cPath,folder)
+
+    deleteFiles(path)
+
+
+    if format == 'MP4':
+        youtubeObject = YouTube(url)
+        youtubeObject = youtubeObject.streams.get_highest_resolution()
+        try:
+            object = youtubeObject.download(path)  
+            print(object)
+            return  object         
+        except:
+            print("Error al descargar MP4")   
+
+    elif format == 'MP3':
+        youtubeObject = YouTube(url)
+        youtubeObject = youtubeObject.streams.filter(only_audio=True).first()
+        try:       
+            out = youtubeObject.download(path)  
+            base,ext = os.path.splitext(out)
+            new = os.path.join(path, youtubeObject.title + '.mp3')  
+            os.rename(out,new)
+            return new
+            
+            
+        except:
+            print("Error al descargar MP3")
+
+def deleteFiles(folder):
+    try:
+        for file in os.listdir(folder):
+            file_path = os.path.join(folder,file)
+
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+        print('Files deleted')
+    except Exception as e:
+        print(f"File cann't be deleted: {e}")
+
+cPath = os.getcwd()
+folder = 'vids'
+path = os.path.join(cPath,folder)
+print(path)
+
+deleteFiles(path)
