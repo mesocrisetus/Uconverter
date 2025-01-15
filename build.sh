d3e38e0ee91cca961d40ab6abe9f89d68186aeb7
Mesocrisetus <alexander.bolivardiaz@gmail.com>
2025-01-15 18:57:23 -0500

    more poke changes

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # Descargar nodejs
-curl -fsSL https://deb.nodesource.com/setup_20.x | sudo bash -
+curl -fsSL https://deb.nodesource.com/setup_20.x | bash -
 sudo apt-get install -y nodejs
